0facd7d2dc6fff850a395a2490f9ad3f8d9bd93b
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-19 22:52:24 -0400

    First commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+import TextInput from './TextInput';
+import TodoList from './TodoList';
+
+export default class App extends Component {
+
+  constructor(props){
+  	super(props);
+  	this.state={
+  		title: 'hello world',
+      todos: [{id: 1,text: 'first thing to do.'}]
+  	}
+  }
+
+  render() {
+    return (
+      <div>
+        <TextInput/>
+        <TodoList todos={this.state.todos}/>
+      </div>
+    );
+  }
+};

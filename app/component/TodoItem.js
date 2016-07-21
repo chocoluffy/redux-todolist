31d72a2f7965fa310890f743ca5b582967e24fee
chocoluffy <luffy.yu@mail.utoronto.ca>
2016-07-20 23:52:14 -0400

    Add TodoItem

---
+++ app/component/TodoItem.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react'
+import actions from '../action/actions'
+
+class TodoItem extends Component {
+	// constructor(props){
+	// 	super(props);
+	// 	this._onComplete = this._onComplete.bind(this);
+	// 	this._onDelete = this._onDelete.bind(this);
+	// }
+
+	_onComplete(){
+		this.props.dispatch(actions.completeTodo(this.props.todo.id))
+	}
+
+	_onDelete(){
+		this.props.dispatch(actions.removeTodo(this.props.todo.id))
+	}
+
+	render(){
+		console.log(this);
+		return (
+			<li>
+				<div>{this.props.todo.text}</div>
+			</li>
+		)
+	}
+}
+
+export default TodoItem
